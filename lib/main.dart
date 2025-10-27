be4c163709148bc8a97637bc584c5b5b4f6fc6e2
erfan <erfanbanaei110@gmail.com>
2025-10-27 18:49:02 +0330

    Refactor main.dart to improve app structure and layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:coffeeapp/constants/constants.dart';
 import 'package:coffeeapp/pages/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:device_frame/device_frame.dart';
 
 void main() {
   runApp(const MainApp());
@@ -13,13 +14,29 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: "Poppins",
-        primaryColor: Cprimary,
-        scaffoldBackgroundColor: Cprimary,
-        textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.white)),
+      home: Scaffold(
+        backgroundColor: Colors.white38,
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: DeviceFrame(
+              device: Devices.ios.iPhone13,
+              screen: MaterialApp(
+                debugShowCheckedModeBanner: false,
+                theme: ThemeData(
+                  fontFamily: "Poppins",
+                  primaryColor: Cprimary,
+                  scaffoldBackgroundColor: Cprimary,
+                  textTheme: const TextTheme(
+                    bodyMedium: TextStyle(color: Colors.white),
+                  ),
+                ),
+                home: const SplashPage(),
+              ),
+            ),
+          ),
+        ),
       ),
-      home: const SplashPage(),
     );
   }
 }
